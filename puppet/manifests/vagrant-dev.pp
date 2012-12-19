128ff7c28c265614d545960b48a533978bb90c37
Hugo Corbucci <hugo.corbucci@thoughtworks.com>
2012-12-19 17:43:44 -0200

    Fixing vagrant config to run the dev box.
    The app is up now once we do vagrant up dev. It uses webrick.

--- puppet/manifests/vagrant-dev.pp
+++ puppet/manifests/vagrant-dev.pp
@@ -1,36 +1,54 @@
 node default {
 	exec { 'update':
-	    command => "apt-get update",
-	    path => "/usr/bin",
+	    command => 'apt-get update',
+	    path => '/usr/bin',
 	}
 
-	package { "git-core":
-	  ensure => "present",
+	package { 'git-core':
+	  ensure => 'present',
 	}
 
-	package { "sqlite3":
-	  ensure => "present",
+	package { 'sqlite3':
+	  ensure => 'present',
 	}
 
-	package { "libsqlite3-dev":
-	  ensure => "present",
-	  require => Package["sqlite3"],
+	package { 'libsqlite3-dev':
+	  ensure => 'present',
+	  require => Package['sqlite3'],
+	}
+
+	package { 'libmysql-ruby1.9.1': 
+		ensure => 'present',
+		require => Package['ruby1.9.3'],
+	}
+
+	#required for mysql2 gem
+	package { 'libmysqlclient-dev':
+		ensure => 'present',
 	}
 
 	class { 'rails-app':
-	  app_name => "registrations",
-	  domain => "agilebrazil.com",
+	  app_name => 'registrations',
+	  domain => 'agilebrazil.com',
 	}
 
 	exec { 'bundle install':
-		path => "/usr/local/bin",
-		cwd => "/srv/apps/registrations/current",
-		require => [Class['rails-app'], Package['sqlite3']]
+		path => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+		command => 'bundle install --path vendor/bundle',
+		cwd => '/srv/apps/registrations/current',
+		user => 'vagrant',
+		logoutput => true,
+		require => [Class['rails-app'], Package['sqlite3'], Package['libmysql-ruby1.9.1'], Package['libmysqlclient-dev'], Package['git-core']]
 	}
 
-	exec { 'rails server -p 9292 &':
-		path => "/srv/apps/registrations/current/script",
-		cwd => "/srv/apps/registrations/current",
-		require => Exec['bundle install']
+	service { 'rails server':
+		ensure => 'running',
+		hasrestart => false,
+		hasstatus => false,
+		provider => 'base',
+		start => '/usr/bin/ruby1.9.3 /srv/apps/registrations/current/script/rails server -p 9292 &',
+		stop => '/usr/bin/pkill ruby1.9.3',
 	}
+
+	Exec['bundle install'] -> Service['rails server']
 }

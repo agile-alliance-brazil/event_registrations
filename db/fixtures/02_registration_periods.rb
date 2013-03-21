69007f2c7fc47462daf12c505356aa5f5ec5eb95
Hugo Corbucci <hugo.corbucci@thoughtworks.com>
2013-03-21 13:46:04 -0500

    Changing dates for early birds and super early birds.

--- db/fixtures/02_registration_periods.rb
+++ db/fixtures/02_registration_periods.rb
@@ -4,22 +4,22 @@ RegistrationPeriod.seed do |period|
   period.event_id = 1
   period.title = 'registration_period.super_early_bird'
   period.start_at = Time.zone.local(2013, 2, 1)
-  period.end_at = Time.zone.local(2013, 3, 14).end_of_day
+  period.end_at = Time.zone.local(2013, 3, 31).end_of_day
 end
 
 RegistrationPeriod.seed do |period|
   period.id = 2
   period.event_id = 1
   period.title = 'registration_period.early_bird'
-  period.start_at = Time.zone.local(2013, 3, 1)
-  period.end_at = Time.zone.local(2013, 4, 1).end_of_day
+  period.start_at = Time.zone.local(2013, 4, 1)
+  period.end_at = Time.zone.local(2013, 4, 30).end_of_day
 end
 
 RegistrationPeriod.seed do |period|
   period.id = 3
   period.event_id = 1
   period.title = 'registration_period.regular'
-  period.start_at = Time.zone.local(2013, 4, 1)
+  period.start_at = Time.zone.local(2013, 5, 1)
   period.end_at = Time.zone.local(2013, 5, 31).end_of_day
 end
 

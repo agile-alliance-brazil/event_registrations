df7cf95747635c12337e50998bcc393d36f0706d
Hugo Corbucci <hugo.corbucci@gmail.com>
2013-03-27 00:26:07 -0500

    Fixing gaps in registration periods.
    Really.. committing late is stupid.

--- db/fixtures/02_registration_periods.rb
+++ db/fixtures/02_registration_periods.rb
@@ -11,15 +11,15 @@ RegistrationPeriod.seed do |period|
   period.id = 2
   period.event_id = 1
   period.title = 'registration_period.early_bird'
-  period.start_at = Time.zone.local(2013, 4, 1)
-  period.end_at = Time.zone.local(2013, 4, 30).end_of_day
+  period.start_at = Time.zone.local(2013, 3, 27)
+  period.end_at = Time.zone.local(2013, 4, 15).end_of_day
 end
 
 RegistrationPeriod.seed do |period|
   period.id = 3
   period.event_id = 1
   period.title = 'registration_period.regular'
-  period.start_at = Time.zone.local(2013, 5, 1)
+  period.start_at = Time.zone.local(2013, 4, 16)
   period.end_at = Time.zone.local(2013, 5, 31).end_of_day
 end
 

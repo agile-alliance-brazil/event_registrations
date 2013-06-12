37951626b18413ff2bc43b9b236c8ca70b09f1b1
Hugo Corbucci <hugo.corbucci@thoughtworks.com>
2013-06-11 20:31:57 -0400

    Adding script to export data about attendances
    This was faster to implement than #29 or #32

---
+++ script/export_csv.rb
@@ -0,0 +1,7 @@
+
+puts Event.all.map do |e|
+  [e.name, 'id,name,registration date,registration fee,city,state,organization,email,phone,twitter,status,events', e.attendances.map do |a|
+    [a.id, a.full_name, a.registration_date, a.registration_fee, a.city, a.state, a.organization, a.email, a.phone, a.twitter_user, a.status, [a.event, a.user.attendances.map(&:event)].flatten.uniq.map(&:name).join("/")]}.map{|a| CSV.generate_line(a)
+  end
+  ]
+end

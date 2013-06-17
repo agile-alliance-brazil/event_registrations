50365c8a6aa0130eb67a9862da55cc167e0438f1
Hugo Corbucci <hugo.corbucci@thoughtworks.com>
2013-06-17 20:04:01 -0300

    Changing end of last period for conference.
    yet another communication mistake

--- db/fixtures/02_registration_periods.rb
+++ db/fixtures/02_registration_periods.rb
@@ -28,14 +28,14 @@ RegistrationPeriod.seed do |period|
   period.event_id = 1
   period.title = 'registration_period.late'
   period.start_at = Time.zone.local(2013, 6, 11)
-  period.end_at = Time.zone.local(2013, 6, 16).end_of_day
+  period.end_at = Time.zone.local(2013, 6, 17).end_of_day
 end
 
 RegistrationPeriod.seed do |period|
   period.id = 5
   period.event_id = 1
   period.title = 'registration_period.last_minute'
-  period.start_at = Time.zone.local(2013, 6, 17)
+  period.start_at = Time.zone.local(2013, 6, 18)
   period.end_at = Time.zone.local(2013, 6, 28).end_of_day
 end
 

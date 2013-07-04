38a62adb6d9cb463d9bae181bc1e81193e838829
Hugo Corbucci <hugo.corbucci@thoughtworks.com>
2013-07-03 23:40:50 -0300

    Fixing test problem
    Because of the if session[:user_id] in the current user method, the
    current user was not being returned for the tests or in cases where the
    session is absent (even if there is a current user set).
    Unlikely to ever happen in production but the logic was flawed anyway
    and was making the tests harder to run.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -25,12 +25,13 @@ class ApplicationController < ActionController::Base
   
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    @current_user
   end
   helper_method :current_user
 
   def current_user= user
     session[:user_id] = user.try(:id)
-    @current_user= user
+    @current_user = user
   end
 
   def default_url_options(options={})

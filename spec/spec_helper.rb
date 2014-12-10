1b37536b5d671b81e877b837f236273b82e323a4
Hugo Corbucci <hugo.corbucci@thoughtworks.com>
2014-12-10 12:16:53 -0900

    Adding codeclimate test coverage

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -15,6 +15,8 @@ Spork.prefork do
   require 'mocha/api'
   require 'cancan/matchers'
   require 'shoulda-matchers'
+  require 'codeclimate-test-reporter'
+  CodeClimate::TestReporter.start
 
   # Requires supporting ruby files with custom matchers and macros, etc,
   # in spec/support/ and its subdirectories.

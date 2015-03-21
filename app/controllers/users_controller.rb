e255854443a0695b3d983cad99b492a727c3a619
Celso Martins <celso.av.martins@gmail.com>
2015-03-21 20:15:29 -0300

    added coverage to users_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,9 +18,11 @@ class UsersController < ApplicationController
       redirect_to @user
     else
       flash[:error] = I18n.t('flash.user.edit')
-      render(:edit)
+      render :edit
     end
   end
+
+  private
 
   def resource
     resource_class.find(params[:id])
@@ -31,7 +33,6 @@ class UsersController < ApplicationController
     User
   end
 
-  protected
   def update_user_params
     params.require(:user).permit(:first_name, :last_name, :email, :phone,
       :country, :state, :city, :organization, :twitter_user, :default_locale)

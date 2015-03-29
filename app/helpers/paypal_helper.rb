09168a206d6f3bd62951bae126bff27f30e99652
Hugo Corbucci <hugo.corbucci@thoughtworks.com>
2015-03-29 18:54:31 -0300

    Readding Style/ColonMethodCall and fixing it
    This ties back to #72

--- app/helpers/paypal_helper.rb
+++ app/helpers/paypal_helper.rb
@@ -38,7 +38,7 @@ module PaypalHelper
   APP_KEY_PEM = File.read("#{Rails.root}/certs/app_key.pem")
   
   def encrypt_for_paypal(values)
-    signed = OpenSSL::PKCS7::sign(OpenSSL::X509::Certificate.new(APP_CERT_PEM), OpenSSL::PKey::RSA.new(APP_KEY_PEM, ''), values.map { |k, v| "#{k}=#{v}" }.join("\n"), [], OpenSSL::PKCS7::BINARY)
-    OpenSSL::PKCS7::encrypt([OpenSSL::X509::Certificate.new(PAYPAL_CERT_PEM)], signed.to_der, OpenSSL::Cipher::Cipher::new("DES3"), OpenSSL::PKCS7::BINARY).to_s.gsub("\n", "")
+    signed = OpenSSL::PKCS7.sign(OpenSSL::X509::Certificate.new(APP_CERT_PEM), OpenSSL::PKey::RSA.new(APP_KEY_PEM, ''), values.map { |k, v| "#{k}=#{v}" }.join("\n"), [], OpenSSL::PKCS7::BINARY)
+    OpenSSL::PKCS7.encrypt([OpenSSL::X509::Certificate.new(PAYPAL_CERT_PEM)], signed.to_der, OpenSSL::Cipher::Cipher.new("DES3"), OpenSSL::PKCS7::BINARY).to_s.gsub("\n", "")
   end
 end

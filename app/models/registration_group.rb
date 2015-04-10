f03042fd0e89f19929cf065c6195357b5af76683
Celso Martins <celso.av.martins@gmail.com>
2015-04-09 23:37:30 -0300

    Changed coupling with bcash adapter and attendance. There is more work to do. Infrastructure to group creation

--- app/models/registration_group.rb
+++ app/models/registration_group.rb
@@ -5,8 +5,6 @@ class RegistrationGroup < ActiveRecord::Base
   has_many :attendances
   has_many :invoices
 
-  has_many :invoices
-
   validates :event, presence: true
 
   before_create :generate_token

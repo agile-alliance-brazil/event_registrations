0eb2451c15e9f2946c95d6d115f488a442f770e3
Celso Martins <celso.av.martins@gmail.com>
2015-04-16 14:49:10 -0300

    added test coverage for pagseguro helper

--- app/helpers/pag_seguro_helper.rb
+++ app/helpers/pag_seguro_helper.rb
@@ -6,16 +6,6 @@ module PagSeguroHelper
     build_config_vars(invoice, notify_url, return_url)
   end
 
-  def add_pag_seguro_config_vars(values, return_url, notify_url)
-    values.tap do |vars|
-      vars[:email] = AppConfig[:pag_seguro][:email]
-      vars[:token] = AppConfig[:pag_seguro][:token]
-      vars[:url_aviso] = notify_url
-      vars[:currency] = 'BRL'
-      vars[:redirect_time] = 5
-    end
-  end
-
   private
 
   def build_config_vars(invoice, notify_url, return_url)
@@ -23,5 +13,14 @@ module PagSeguroHelper
         PagSeguroAdapter.from_invoice(invoice).to_variables,
         return_url, notify_url
     )
+  end
+
+  def add_pag_seguro_config_vars(values, return_url, notify_url)
+    values.tap do |vars|
+      vars[:email] = AppConfig[:pag_seguro][:email]
+      vars[:token] = AppConfig[:pag_seguro][:token]
+      vars[:url_aviso] = notify_url
+      vars[:currency] = 'BRL'
+    end
   end
 end

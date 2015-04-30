301cbf0694b6c490fb0c18c2fe138cc7dc6d8569
Willian Arantes <warantesbr@gmail.com>
2015-04-30 06:39:09 -0300

    Adding test to pag_seguro notification

--- spec/controllers/payment_notifications_controller_spec.rb
+++ spec/controllers/payment_notifications_controller_spec.rb
@@ -1,5 +1,6 @@
 # encoding: UTF-8
 require 'spec_helper'
+require 'vcr_setup'
 
 describe PaymentNotificationsController, type: :controller do
   describe "POST create" do
@@ -22,6 +23,16 @@ describe PaymentNotificationsController, type: :controller do
         post :create, type: 'bcash', status: "Aprovada", transacao_id: "12345678",
                       pedido: @attendance.id, secret: APP_CONFIG[:bcash][:secret]
       end.to change(PaymentNotification, :count).by(1)
+    end
+
+    it "should create PaymentNotification with pag_seguro type" do
+      VCR.use_cassette('pag_seguro_notification_paid', :record => :once, :match_requests_on => [:path]) do
+        expect do
+          post :create, type: 'pag_seguro', pedido: @attendance.id, :"notificationType" => "transaction",
+                        :"notificationCode" => "VALID-NOTIFICATION-CODE",
+                        store_code: APP_CONFIG[:bcash][:store_code]
+        end.to change(PaymentNotification, :count).by(1)
+      end
     end
   end
 end

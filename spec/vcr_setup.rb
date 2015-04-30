7f484d9400ad7978efcbc153cd5ca562ffc2d9b9
Willian Arantes <warantesbr@gmail.com>
2015-04-30 12:49:37 -0300

    Enabling WebMock on demand

--- spec/vcr_setup.rb
+++ spec/vcr_setup.rb
@@ -1,6 +1,6 @@
-# require 'vcr'
+require 'vcr'
 
-# VCR.configure do |config|
-#   config.cassette_library_dir = 'spec/vcr_cassettes'
-#   config.hook_into :webmock
-# end
+VCR.configure do |config|
+  config.cassette_library_dir = 'spec/vcr_cassettes'
+  config.hook_into :webmock
+end

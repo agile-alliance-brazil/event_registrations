84eb049dd98e0fc5a0f6866ef12d1d753519d7c4
Celso Martins <celso.av.martins@gmail.com>
2015-06-12 00:15:24 -0300

    now group discount takes priority over discount from AA membership.

--- app/controllers/concerns/groupable.rb
+++ app/controllers/concerns/groupable.rb
@@ -6,7 +6,7 @@ module Concerns
       group = @event.registration_groups.find_by_token(params['registration_token'])
       @attendance.registration_group = group if group.present? && group.accept_members?
 
-      return unless AgileAllianceService.check_member(@attendance.email) && !group.try(:free?)
+      return unless AgileAllianceService.check_member(@attendance.email) && !group.present?
       group = RegistrationGroup.find_by(name: 'Membros da Agile Alliance')
       @attendance.registration_group = group
       @attendance.accept

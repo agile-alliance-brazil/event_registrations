a17c32298c5572f4239cdb3daa8dfb22b2c7f02e
Celso Martins <celso.av.martins@gmail.com>
2015-10-05 12:01:15 -0300

    fixed duplicity in group report

--- app/services/generate_payment_type_report.rb
+++ app/services/generate_payment_type_report.rb
@@ -1,11 +1,11 @@
 class GeneratePaymentTypeReport
   def self.run_for(event)
     return {} unless event.present?
-    event.attendances.already_paid.joins(:invoices).group('invoices.payment_type').sum(:registration_value)
+    event.attendances.already_paid.select(:attendance).joins(:invoices).group('invoices.payment_type').sum(:registration_value)
   end
 
   def self.count_for(event)
     return {} unless event.present?
-    event.attendances.already_paid.joins(:invoices).group('invoices.payment_type').count(:id)
+    event.attendances.already_paid.select(:attendance).joins(:invoices).group('invoices.payment_type').count(:id)
   end
 end

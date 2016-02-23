18c7b634a4bfc65637b498446288e805cdac2787
Hugo Corbucci <hugo.corbucci@thoughtworks.com>
2016-02-23 10:01:32 -0800

    Upgrading patch level gems
    Also removing the path from the dev.sh script so as to match each
    developer's favorite bundler config

--- dev.sh
+++ dev.sh
@@ -3,5 +3,5 @@
 (brew --version &> /dev/null) && (brew install mysql &> /dev/null) && (brew tap homebrew/services &> /dev/null) && (brew services start mysql &> /dev/null)
 (apt-get --version &> /dev/null) && (apt-get install -y mysql-server &> /dev/null) && (service mysql start &> /dev/null)
 echo "GRANT ALL PRIVILEGES ON * . * TO 'registrations_db'@'localhost';" | mysql -u root
-bundle install --path=vendor/bundle
+bundle install
 bundle exec foreman start -f Procfile.dev

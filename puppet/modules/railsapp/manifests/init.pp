a4daf1c5e194f144718c62f03ae799c4214b037b
Hugo Corbucci <hugo.corbucci@thoughtworks.com>
2016-05-07 14:15:41 -0700

    Upgrading to ruby 2.3.1

--- puppet/modules/railsapp/manifests/init.pp
+++ puppet/modules/railsapp/manifests/init.pp
@@ -7,8 +7,7 @@ class railsapp( $user, $app_name ) {
     rvm_system_ruby { 'ruby-2.3.0':
         name        => 'ruby-2.3.0',
         ensure      => 'present',
-        build_opts  => '--disable-binary',
-        default_use => true
+        build_opts  => '--disable-binary'
     }
 
     rvm_gem { 'bundler23':
@@ -21,7 +20,8 @@ class railsapp( $user, $app_name ) {
     rvm_system_ruby { 'ruby-2.3.1':
         name        => 'ruby-2.3.1',
         ensure      => 'present',
-        build_opts  => '--disable-binary'
+        build_opts  => '--disable-binary',
+        default_use => true
     }
 
     rvm_gem { 'bundler231':

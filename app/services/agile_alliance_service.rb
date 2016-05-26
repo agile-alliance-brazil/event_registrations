5b584ebb1582de8c2562a3cd01e7908d888fb2b9
Celso Martins <celso.av.martins@gmail.com>
2016-05-26 19:48:39 -0300

    fixed the response answer for service bus integration

--- app/services/agile_alliance_service.rb
+++ app/services/agile_alliance_service.rb
@@ -2,6 +2,6 @@ module AgileAllianceService
   def self.check_member(email)
     url = "#{APP_CONFIG[:agile_alliance][:api_host]}/check_member/#{email}"
     response = HTTParty.get(url, headers: { 'AUTHORIZATION' => APP_CONFIG[:agile_alliance][:api_token] })
-    response.present? && JSON.parse(response.parsed_response)['member']
+    response.present? && JSON.parse(response.body)['member']
   end
 end
--- spec/services/agile_alliance_service_spec.rb
+++ spec/services/agile_alliance_service_spec.rb
@@ -2,14 +2,18 @@ RSpec.describe AgileAllianceService do
   before { WebMock.enable! }
   after { WebMock.disable! }
 
+  let(:token) { APP_CONFIG[:agile_alliance][:api_token] }
+  let(:headers) { { 'Authorization' => token } }
+  let(:host) { "#{APP_CONFIG[:agile_alliance][:api_host]}/check_member/bla" }
+
   describe '.check_member' do
     context 'when the user is a member' do
-      before { WebMock.stub_request(:get, "#{APP_CONFIG[:agile_alliance][:api_host]}/check_member/bla").with(headers: { 'Authorization' => APP_CONFIG[:agile_alliance][:api_token] }).to_return(status: 200, body: { member: true }.to_json, headers: {}) }
+      before { WebMock.stub_request(:get, host).with(headers: headers).to_return(body: { member: true }.to_json, headers: {}) }
       it { expect(AgileAllianceService.check_member('bla')).to be_truthy }
     end
 
     context 'when the user is not a member' do
-      before { WebMock.stub_request(:get, "#{APP_CONFIG[:agile_alliance][:api_host]}/check_member/bla").with(headers: { 'Authorization' => APP_CONFIG[:agile_alliance][:api_token] }).to_return(status: 200, body: { member: false }.to_json, headers: {}) }
+      before { WebMock.stub_request(:get, host).with(headers: headers).to_return(body: { member: false }.to_json, headers: {}) }
       it { expect(AgileAllianceService.check_member('bla')).to be_falsey }
     end
   end

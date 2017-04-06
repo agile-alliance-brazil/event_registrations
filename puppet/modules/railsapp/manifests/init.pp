3aa848e72f8d9efe9e35b00ea2493a72a9222c4c
Hugo Corbucci <hugo.corbucci@gmail.com>
2017-04-05 20:52:22 -0400

    Adding ruby 2.4.0 to puppet

--- puppet/modules/railsapp/manifests/init.pp
+++ puppet/modules/railsapp/manifests/init.pp
@@ -8,6 +8,13 @@ class railsapp( $user, $app_name ) {
         name        => 'ruby-2.3.1',
         ensure      => 'present',
         build_opts  => '--disable-binary',
+        default_use => false
+    }
+
+    rvm_system_ruby { 'ruby-2.4.0':
+        name        => 'ruby-2.4.0',
+        ensure      => 'present',
+        build_opts  => '--disable-binary',
         default_use => true
     }
 
@@ -16,6 +23,13 @@ class railsapp( $user, $app_name ) {
         ruby_version => 'ruby-2.3.1@global',
         ensure       => latest,
         require      => Rvm_system_ruby['ruby-2.3.1'];
+    }
+
+    rvm_gem { 'bundler240':
+        name         => 'bundler',
+        ruby_version => 'ruby-2.4.0@global',
+        ensure       => latest,
+        require      => Rvm_system_ruby['ruby-2.4.0'];
     }
   }
 

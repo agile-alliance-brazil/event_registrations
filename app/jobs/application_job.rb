385c7acb600172795def93272dd04a543211ab22
Hugo Corbucci <hugo.corbucci@gmail.com>
2017-06-05 08:57:53 -0400

    Adding top level documentation for the future.

--- app/jobs/application_job.rb
+++ app/jobs/application_job.rb
@@ -1,2 +1,4 @@
+# ApplicationJob is an extra layer of indirection to ActiveJob::Base which
+# allows us to customize its behavior per application without monkey patching
 class ApplicationJob < ActiveJob::Base
 end
--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,6 @@
+# ApplicationRecord is an extra layer of indirection to ActiveRecord::Base
+# which allows us to customize its behavior per application without monkey
+# patching
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
 end

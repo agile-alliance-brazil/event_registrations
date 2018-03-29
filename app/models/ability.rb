e4c865d658c473e067859d425f35ac03c7ceee1e
Celso Martins <celso.av.martins@gmail.com>
2018-03-29 11:17:01 -0300

    fixed permissions to register to the event

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -20,12 +20,14 @@ class Ability
     can(:read, 'static_pages')
     can(:manage, 'password_resets')
     can(:read, Event)
+    can(%i[new create], AttendancesController)
+    can(%i[edit update], AttendancesController) { |attendance| attendance.user_id == @user.id || attendance.email == @user.email }
+
     can(:manage, @user)
     can(%i[show destroy], Attendance) { |attendance| attendance.user_id == @user.id || attendance.email == @user.email }
 
     can do |action, subject_class, _subject|
-      expand_actions([:create]).include?(action) && [Attendance].include?(subject_class) &&
-        Time.zone.now <= @event.end_date
+      expand_actions([:create]).include?(action) && [Attendance].include?(subject_class) && Time.zone.now <= @event.end_date
     end
   end
 

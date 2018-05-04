a6def4a854a274ea229b5494b8da0ae58a25b42a
Celso Martins <celso.av.martins@gmail.com>
2018-05-04 20:38:47 -0300

    fixed javascript to attendance search

--- app/assets/javascripts/attendances/index.js
+++ app/assets/javascripts/attendances/index.js
@@ -2,10 +2,11 @@ function submitSearch(event_id) {
     var pending = $('#pending').is(':checked');
     var accepted = $('#accepted').is(':checked');
     var paid = $('#paid').is(':checked');
+    var confirmed = $('#confirmed').is(':checked');
     var showed_in = $('#showed_in').is(':checked');
     var cancelled = $('#cancelled').is(':checked');
 
     var searchText = $('#search').val();
 
-    $.get('/events/' + event_id + '/attendances/search', { event_id: event_id, search: searchText, pending: pending, accepted: accepted, paid: paid, showed_in: showed_in, cancelled: cancelled } );
+    $.get('/events/' + event_id + '/attendances/search', { event_id: event_id, search: searchText, pending: pending, accepted: accepted, paid: paid, confirmed: confirmed, showed_in: showed_in, cancelled: cancelled } );
 }

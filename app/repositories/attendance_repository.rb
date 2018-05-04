b34c94df28cf30707da1006b07151a4fe8761121
Celso Martins <celso.av.martins@gmail.com>
2018-05-04 19:49:10 -0300

    trying to fix weird problem in search on the production dada - did not reproduce locally

--- app/repositories/attendance_repository.rb
+++ app/repositories/attendance_repository.rb
@@ -5,7 +5,7 @@ class AttendanceRepository
 
   def search_for_list(event, text, statuses)
     statuses_keys = statuses.map { |status| Attendance.statuses[status] }
-    Attendance.where('event_id = ? AND ((first_name LIKE ? OR last_name LIKE ? OR organization LIKE ? OR email LIKE ? OR id = ?) AND attendances.status IN (?))', event.id, "%#{text}%", "%#{text}%", "%#{text}%", "%#{text}%", text.to_s, statuses_keys).order(updated_at: :desc)
+    event.attendances.where(status: statuses_keys).where('((first_name LIKE :first_name OR last_name LIKE :last_name OR organization LIKE :organization OR email LIKE :email OR attendances.id = :attendance_id))', first_name: "%#{text}%", last_name: "%#{text}%", organization: "%#{text}%", email: "%#{text}%", attendance_id: text.to_s).order(updated_at: :desc)
   end
 
   def for_cancelation_warning(event)

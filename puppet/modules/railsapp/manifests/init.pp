ee52b82f2b88adbb8cecee57809406baf57812dd
Celso Martins <celso.av.martins@gmail.com>
2018-05-22 16:53:24 -0300

    fixed configuration to migrate ruby in puppet

--- puppet/modules/railsapp/manifests/init.pp
+++ puppet/modules/railsapp/manifests/init.pp
@@ -8,14 +8,28 @@ class railsapp( $user, $app_name ) {
         name        => 'ruby-2.5.1',
         ensure      => 'present',
         build_opts  => '--disable-binary',
+        default_use => false
+    }
+
+    rvm_gem { 'bundler251':
+        name         => 'bundler',
+        ruby_version => 'ruby-2.5.1@global',
+        ensure       => '1.16.2',
+        require      => Rvm_system_ruby['ruby-2.5.1'];
+    }
+
+    rvm_system_ruby { 'ruby-2.4.3':
+        name        => 'ruby-2.4.3',
+        ensure      => 'present',
+        build_opts  => '--disable-binary',
         default_use => true
     }
 
     rvm_gem { 'bundler243':
         name         => 'bundler',
-        ruby_version => 'ruby-2.5.1@global',
-        ensure       => '1.16.0',
-        require      => Rvm_system_ruby['ruby-2.5.1'];
+        ruby_version => 'ruby-2.4.3@global',
+        ensure       => '1.16.2',
+        require      => Rvm_system_ruby['ruby-2.4.3'];
     }
   }
 

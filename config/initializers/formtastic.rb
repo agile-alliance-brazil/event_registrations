6d6f4a92db9b87b79a96cd6f7c350763b5e3d5bb
Celso Martins <celso.av.martins@gmail.com>
2019-01-03 11:27:57 -0200

    fixed partials for each status information

--- config/initializers/formtastic.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-begin
-  Formtastic::FormBuilder.escape_html_entities_in_hints_and_labels = false
-  Formtastic::FormBuilder.action_class_finder = Formtastic::ActionClassFinder
-  Formtastic::FormBuilder.input_class_finder = Formtastic::InputClassFinder
-rescue LoadError
-  Rails.logger.error("Formstatic isn't loaded! Either remove this initializer or ensure Formtastic is loaded")
-end

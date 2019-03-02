6d515894df6777bf5478d3d280ba3af4fca54ebe
Hugo Corbucci <hugo.corbucci@gmail.com>
2019-03-02 11:34:09 -0500

    refactor omni auth callback

--- app/controllers/devise_custom/omniauth_callbacks_controller.rb
+++ app/controllers/devise_custom/omniauth_callbacks_controller.rb
@@ -24,14 +24,18 @@ module DeviseCustom
       @user = User.from_omniauth(request.env['omniauth.auth'])
 
       if @user.persisted?
-        return redirect_to edit_default_password_user_path(@user) if @user.sign_in_count.zero?
-
-        sign_in_and_redirect(@user, event: :authentication)
-        set_flash_message(:notice, :success, kind: omniauth_provider) if is_navigational_format?
+        complete_login(omniauth_provider)
       else
         session[omniauth_session] = request.env['omniauth.auth'].except('extra')
         redirect_to new_user_registration_url
       end
+    end
+
+    def complete_login(omniauth_provider)
+      return redirect_to edit_default_password_user_path(@user) if @user.sign_in_count.zero?
+
+      sign_in_and_redirect(@user, event: :authentication)
+      set_flash_message(:notice, :success, kind: omniauth_provider) if is_navigational_format?
     end
   end
 end

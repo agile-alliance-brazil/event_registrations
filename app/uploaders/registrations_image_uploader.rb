db7dd2227de0fa7e71847efb9495e10b92818109
Celso Martins <celso.av.martins@gmail.com>
2019-03-07 18:00:22 -0300

    fixed image size to event cards

--- app/uploaders/registrations_image_uploader.rb
+++ app/uploaders/registrations_image_uploader.rb
@@ -13,6 +13,14 @@ class RegistrationsImageUploader < CarrierWave::Uploader::Base
     process resize_to_fit: [50, 50]
   end
 
+  version :resized do
+    # returns an image with a maximum width of 100px
+    # while maintaining the aspect ratio
+    # 10000 is used to tell CW that the height is free
+    # and so that it will hit the 100 px width first
+    process resize_to_fit: [600, -1]
+  end
+
   def extension_whitelist
     %w[jpg jpeg gif png]
   end

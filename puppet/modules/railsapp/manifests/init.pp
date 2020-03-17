aa61a3266d2bb4bc0ad9b656632b03dc436172fe
Hugo Corbucci <hugo.corbucci@gmail.com>
2020-03-17 12:44:02 -0300

    and obviously we'll need to force bundler without no-rdoc too

--- puppet/modules/railsapp/manifests/init.pp
+++ puppet/modules/railsapp/manifests/init.pp
@@ -36,6 +36,7 @@ class railsapp( $user, $app_name ) {
         name         => 'bundler',
         ruby_version => 'ruby-2.6.4@global',
         ensure       => '1.17.3',
+        source       => "https://rubygems.org/",
         require      => Rvm_gem['rubygems308'];
     }
   }

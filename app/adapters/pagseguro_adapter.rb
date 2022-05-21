4f78b69812ff5529c375c3ff9a7c94bb898ea0cc
Celso Martins <celso.av.martins@gmail.com>
2022-05-20 23:54:44 -0300

    feat: ignore when the attendance is already paid

--- app/adapters/pagseguro_adapter.rb
+++ app/adapters/pagseguro_adapter.rb
@@ -13,6 +13,7 @@ class PagseguroAdapter
         Rails.logger.info("#{invoice_params['code']},#{invoice_params['reference']},#{invoice_params['status']},#{invoice_params['grossAmount']},#{invoice_params['date']}")
 
         attendance = Attendance.find_by(id: invoice_params['reference'])
+        next if attendance.blank?
 
         invoice = Invoice.where(attendance: attendance, transaction_id: invoice_params['code']).first_or_create
         invoice.update(settle_amount: invoice_params['grossAmount'], status: invoice_params['status'].to_i, invoice_date: invoice_params['date'])
@@ -24,6 +25,8 @@ class PagseguroAdapter
   private
 
   def process_valid_invoice(attendance, invoice, invoice_params)
+    return unless attendance.pending? || attendance.accepted?
+
     if invoice.valid?
       Rails.logger.info("Invoice #{invoice_params['reference']} processed.")
 
--- spec/adapters/pagseguro_adapter_spec.rb
+++ spec/adapters/pagseguro_adapter_spec.rb
@@ -6,16 +6,17 @@ RSpec.describe PagseguroAdapter, type: :adapter do
       Fabricate :attendance, id: 4, status: :pending, registration_value: 1
       cancelled = Fabricate :attendance, id: 3, status: :pending, registration_value: 1
       Fabricate :attendance, id: 8644, status: :pending, registration_value: 460
-      Fabricate :attendance, id: 8677, status: :pending, registration_value: 460
-      Fabricate :attendance, id: 8647, status: :pending, registration_value: 460
-      Fabricate :attendance, id: 8669, status: :pending, registration_value: 460
-      Fabricate :attendance, id: 8668, status: :pending, registration_value: 460
-      Fabricate :attendance, id: 8655, status: :pending, registration_value: 460
-      # Fabricate :attendance, id: 8654, status: :pending, registration_value: 460
+      Fabricate :attendance, id: 8677, status: :accepted, registration_value: 460
+      Fabricate :attendance, id: 8647, status: :waiting, registration_value: 460
+      Fabricate :attendance, id: 8669, status: :cancelled, registration_value: 460
+      Fabricate :attendance, id: 8668, status: :confirmed, registration_value: 460
+      Fabricate :attendance, id: 8655, status: :paid, registration_value: 460
       paid_less = Fabricate :attendance, id: 8651, status: :pending, registration_value: 470
       no_invoice = Fabricate :attendance, id: 976, status: :pending, registration_value: 470
 
       response = file_fixture('pag_seguro_invoices_response.json').read
+
+      expect(EmailNotificationsMailer).to(receive(:registration_paid)).exactly(3).times.and_call_original
 
       described_class.instance.read_pag_seguro_body(JSON.parse(response))
 
@@ -26,7 +27,7 @@ RSpec.describe PagseguroAdapter, type: :adapter do
       expect(paid_less.reload.pending?).to be true
       expect(no_invoice.reload.pending?).to be true
 
-      expect(Attendance.paid.count).to eq 6
+      expect(Attendance.paid.count).to eq 4
     end
   end
 end
